b1b176b9839df97e15ce70950a3642c8a897d629
Mal <melk.goo@gmail.com>
2025-07-30 08:32:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 st.title('Penguin Prediction App')
+st.info('This is end to end app')
+
+with st.expander("data"):
+    pass
