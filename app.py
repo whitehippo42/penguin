928d3ffccb77622715aa2e7a5a93bdad17908ff7
Mal <melk.goo@gmail.com>
2025-07-30 08:56:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -22,5 +22,9 @@ with st.sidebar:
     st.header("Input Vars")
     island = st.selectbox('Island',('Biscoe','Dream','Torgersen'))
     bill_length_nm =st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+    bill_depth_nm =st.slider('Bill depth (mm)', 13.1, 23.5, 17.2)
+    flipper_length_nm =st.slider('Flipper length (mm)', 172.00, 231.00, 201.00)
+    body_mass_g =st.slider('Body mass (g)', 2700, 6300, 4207)
+    gender =st.selector('Gender', ('male', 'Female'))
 
 
